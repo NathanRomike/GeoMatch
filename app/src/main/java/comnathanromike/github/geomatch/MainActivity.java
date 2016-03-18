be1fbc9b6d659660d460f09de221318f24a03be1
NathanRomike <nathanromike@gmail.com>
2016-03-18 09:42:14 -0700

    Add logo and other styling to toolbar.

--- app/src/main/java/comnathanromike/github/geomatch/MainActivity.java
+++ app/src/main/java/comnathanromike/github/geomatch/MainActivity.java
@@ -23,11 +23,15 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        this.getSupportActionBar().setTitle(R.string.blank_text);
+        this.getSupportActionBar().setDisplayUseLogoEnabled(true);
+        this.getSupportActionBar().setLogo(R.drawable.icon);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "This will open existing camera application!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
