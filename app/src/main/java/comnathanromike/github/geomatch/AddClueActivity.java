a486c9ce25137180a3c9b5e9ea3fa10fb730b817
NathanRomike <nathanromike@gmail.com>
2016-03-18 14:08:37 -0700

    Add method to pass hint from add clues activity to the view clues activity.

--- app/src/main/java/comnathanromike/github/geomatch/AddClueActivity.java
+++ app/src/main/java/comnathanromike/github/geomatch/AddClueActivity.java
@@ -1,9 +1,15 @@
 package comnathanromike.github.geomatch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddClueActivity extends AppCompatActivity {
+    private Button mAddPhotoButton;
+    private EditText mAddHint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +19,20 @@ public class AddClueActivity extends AppCompatActivity {
         this.getSupportActionBar().setTitle(R.string.blank_text);
         this.getSupportActionBar().setDisplayUseLogoEnabled(true);
         this.getSupportActionBar().setLogo(R.drawable.icon);
+
+        mAddPhotoButton = (Button) findViewById(R.id.addPhotoButton);
+        mAddHint = (EditText) findViewById(R.id.photoHintEditText);
+
+        mAddPhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String photoHints = mAddHint.getText().toString();
+                Intent intent = new Intent(AddClueActivity.this, MainActivity.class);
+                intent.putExtra("photoHints", photoHints);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
