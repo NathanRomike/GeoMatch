bccb8a12eae7e6986b938302e6ca53d99b119fea
NathanRomike <nathanromike@gmail.com>
2016-03-28 21:19:15 -0700

    Create detail view layout.

---
+++ app/src/main/java/comnathanromike/github/geomatch/ui/PhotoDetailActivity.java
@@ -0,0 +1,15 @@
+package comnathanromike.github.geomatch.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import comnathanromike.github.geomatch.R;
+
+public class PhotoDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_detail);
+    }
+}
