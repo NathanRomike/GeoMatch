54a892793f7ff12be41d315644fd76ffc7d65463
NathanRomike <nathanromike@gmail.com>
2016-03-28 21:37:28 -0700

    Add fragment layout and [arcelable. No clicklistener yet.

--- app/src/main/java/comnathanromike/github/geomatch/ui/PhotoDetailFragment.java
+++ app/src/main/java/comnathanromike/github/geomatch/ui/PhotoDetailFragment.java
@@ -7,8 +7,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import comnathanromike.github.geomatch.R;
+import comnathanromike.github.geomatch.models.PuzzlePhoto;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,14 +27,8 @@ import comnathanromike.github.geomatch.R;
  * create an instance of this fragment.
  */
 public class PhotoDetailFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    @Bind(R.id.mediumPhotoImageView) ImageView mImageMedium;
+    private PuzzlePhoto mPuzzlePhoto;
 
     private OnFragmentInteractionListener mListener;
 
@@ -34,20 +36,10 @@ public class PhotoDetailFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PhotoDetailFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PhotoDetailFragment newInstance(String param1, String param2) {
+    public static PhotoDetailFragment newInstance(PuzzlePhoto puzzlePhoto) {
         PhotoDetailFragment fragment = new PhotoDetailFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putParcelable("photo", Parcels.wrap(puzzlePhoto));
         fragment.setArguments(args);
         return fragment;
     }
@@ -55,17 +47,17 @@ public class PhotoDetailFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        mImageMedium = Parcels.unwrap(getArguments().getParcelable("photo"));
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_photo_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_photo_detail, container, false);
+        ButterKnife.bind(this, view);
+
+        Picasso.with(view.getContext()).load(mPuzzlePhoto.getMediumPhotoUrl()).into(mImageMedium);
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
