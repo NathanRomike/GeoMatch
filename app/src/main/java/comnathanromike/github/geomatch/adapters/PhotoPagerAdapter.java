293935c45de6b2a326910a2ac0804989ad7cf557
NathanRomike <nathanromike@gmail.com>
2016-03-29 17:58:38 -0700

    Create pagerAdapter for photo detail fragment.

---
+++ app/src/main/java/comnathanromike/github/geomatch/adapters/PhotoPagerAdapter.java
@@ -0,0 +1,38 @@
+package comnathanromike.github.geomatch.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+import comnathanromike.github.geomatch.models.PuzzlePhoto;
+import comnathanromike.github.geomatch.ui.PhotoDetailFragment;
+
+/**
+ * Created by Guest on 3/29/16.
+ */
+public class PhotoPagerAdapter extends FragmentPagerAdapter {
+    private ArrayList<PuzzlePhoto> mPhotos;
+
+    public PhotoPagerAdapter(FragmentManager fm, ArrayList<PuzzlePhoto> photos) {
+        super(fm);
+        mPhotos = photos;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return PhotoDetailFragment.newInstance(mPhotos.get(position));
+    }
+
+    @Override
+    public int getCount() {
+        return mPhotos.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mPhotos.get(position).getTitle();
+    }
+
+}
--- app/src/main/java/comnathanromike/github/geomatch/ui/PhotoDetailFragment.java
+++ app/src/main/java/comnathanromike/github/geomatch/ui/PhotoDetailFragment.java
@@ -18,23 +18,11 @@ import butterknife.ButterKnife;
 import comnathanromike.github.geomatch.R;
 import comnathanromike.github.geomatch.models.PuzzlePhoto;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link PhotoDetailFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link PhotoDetailFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PhotoDetailFragment extends Fragment {
     @Bind(R.id.mediumPhotoImageView) ImageView mImageMedium;
     private PuzzlePhoto mPuzzlePhoto;
 
-    private OnFragmentInteractionListener mListener;
-
-    public PhotoDetailFragment() {
-        // Required empty public constructor
-    }
+    public PhotoDetailFragment() {}
 
     public static PhotoDetailFragment newInstance(PuzzlePhoto puzzlePhoto) {
         PhotoDetailFragment fragment = new PhotoDetailFragment();
@@ -58,44 +46,5 @@ public class PhotoDetailFragment extends Fragment {
 
         Picasso.with(view.getContext()).load(mPuzzlePhoto.getMediumPhotoUrl()).into(mImageMedium);
         return view;
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
